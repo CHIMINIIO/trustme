d568a901b98da2739f3b381a3fd2a971e894b07d
Mattias Burman <mmburman@hotmail.com>
2025-11-28 15:44:49 +0100

    update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,28 @@
 extends CharacterBody2D
 
-@export var speed = 40
+@export var speed = 50.0
+@export var drag  = 1.0
+@export var decel  = 0.0
+
+var is_decelerating = true
 
 func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	if input_direction:
+		is_decelerating = false
 		var k = position + Vector2(-input_direction.y, input_direction.x)
 		look_at(k)
-		velocity += input_direction * speed
-
-
+		velocity += input_direction * speed * ((3/2) - (input_direction.dot(velocity.normalized()))/2)
+	else:
+		is_decelerating = true
+		
+		
 func _physics_process(delta):
-	velocity = (1 - delta*2)*velocity
+	velocity = (1 - delta*drag)*velocity
+	if is_decelerating:
+		velocity = (1 - delta*decel)*velocity
+	
 	get_input()
-	move_and_slide()
+	var collision_info = move_and_collide(velocity*delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
