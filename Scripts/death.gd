504a89de15fbf4f0c202fc33d5c887af55721d67
Mattias Burman <mmburman@hotmail.com>
2025-12-15 22:48:09 +0100

    orthagonal movement for flanker

--- Scripts/death.gd
+++ Scripts/death.gd
@@ -2,23 +2,33 @@ extends Area2D
 
 signal hit
 
-@export var is_flank = false
-@export var speed    = 300
+@export var flanky      = 0.0
+@export var speed       = 300
+@export var sigmoid_div = 100
 
 func sigmoid(x):
-	return ((exp(x)/(exp(x)+1)) - 0.5)
+	return (exp(x)/(exp(x) + 1) - 1/2)*2
+
+func sigmoid_normal(v):
+	return v.normalized()*(sigmoid(sqrt(v.dot(v))))
+
+func wproject(v, w):
+	var x = w.normalized()
+	return v.dot(x)*x
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _process(delta):
-	var target = null
-	if is_flank:
-		target = Globals.player_pos + (Globals.player_vel * 100 * sigmoid(position.distance_to(Globals.player_pos) / 100) * delta)
+	var direct = (Globals.player_pos - position).normalized()
+	var v = Globals.player_vel
+	var k
+	if direct.dot(v) >= 0:
+		k = 0
 	else:
-		target = Globals.player_pos
-		
-	position = position + (target - position).normalized() * delta * speed
+		k = 1
+	var w = ((1 - flanky)*direct + k*flanky*sigmoid_normal(wproject(direct, Vector2(-v.y, v.x))/sigmoid_div)).normalized()
+	position = position + w * delta * speed
 
 func _on_body_entered(_ss):
 	hit.emit()
